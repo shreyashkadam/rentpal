499ab54241fd12d45567b9c7ca2cd4497571c158
Shreyash Kadam <shreyash.kadam10@gmail.com>
2023-05-03 12:32:47 +0530

    Update register modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import { useCallback, useState } from "react";
+import { toast } from "react-hot-toast";
 import { 
   FieldValues, 
   SubmitHandler,
@@ -13,6 +14,9 @@ import {
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 import Modal from "./Modal";
+import Input from "../inputs/Input";
+import Heading from "../Heading";
+import Button from "../Button";
 
 const RegisterModal= () => {
   const registerModal = useRegisterModal();
@@ -37,15 +41,91 @@ const RegisterModal= () => {
 
     axios.post('/api/register', data)
     .then(() => {
+      toast.success('Registered!');
       registerModal.onClose();
     })
     .catch((error) => {
-      toast.error(error);
+      toast.error("Something went wrong!");
     })
     .finally(() => {
       setIsLoading(false);
     })
   }
+
+  const onToggle = useCallback(() => {
+    registerModal.onClose();
+  }, [registerModal])
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading
+        title="Welcome to Airbnb"
+        subtitle="Create an account!"
+      />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button 
+        outline 
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}} 
+      />
+      <Button 
+        outline 
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div 
+        className="
+          text-neutral-500 
+          text-center 
+          mt-4 
+          font-light
+        "
+      >
+        <p>Already have an account?
+          <span 
+            onClick={onToggle} 
+            className="
+              text-neutral-800
+              cursor-pointer 
+              hover:underline
+            "
+            > Log in</span>
+        </p>
+      </div>
+    </div>
+  )
 
   return (
     <Modal
@@ -55,6 +135,8 @@ const RegisterModal= () => {
       actionLabel="Continue"
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
     />
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
+import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata = {
   title: "Airbnb",
@@ -21,8 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-          <RegisterModal/>
-          <Navbar />
+        <ToasterProvider />
+        <RegisterModal />
+        <Navbar />
         {children}
       </body>
     </html>

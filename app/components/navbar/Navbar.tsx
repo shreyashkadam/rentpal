c39d3bb134c935ad8e89296cfd2487fc5500d4b1
Shreyash Kadam <shreyash.kadam10@gmail.com>
2023-05-04 11:32:59 +0530

    Add categories

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,26 +1,29 @@
-"use client";
+import { SafeUser } from "@/app/types";
 
+import Categories from "./Categories";
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
-import { SafeUser } from "@/app/types";
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
 }
 
-const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
-  return (
+const Navbar: React.FC<NavbarProps> = ({
+  currentUser,
+}) => {
+  return ( 
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div
         className="
-            py-4
-            border-b-[1px]"
+          py-4 
+          border-b-[1px]
+        "
       >
-        <Container>
-          <div
-            className="
+      <Container>
+        <div 
+          className="
             flex 
             flex-row 
             items-center 
@@ -28,15 +31,17 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
             gap-3
             md:gap-0
           "
-          >
-            <Logo />
-            <Search />
-            <UserMenu currentUser={currentUser}/>
-          </div>
-        </Container>
-      </div>
+        >
+          <Logo />
+          <Search />
+          <UserMenu currentUser={currentUser} />
+        </div>
+      </Container>
     </div>
+    <Categories />
+  </div>
   );
-};
+}
+
 
 export default Navbar;
